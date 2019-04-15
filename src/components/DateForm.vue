5fc11a9cf6f04044a1a0a2d50ec8d9a5b574be6d
Eric Weissman <eric.weissman21@gmail.com>
2019-04-15 14:15:32 -0600

    Creates initial layout components

---
+++ src/components/DateForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <form class="date-form">
+    <p>choose a date.</p>
+    <input type="text" placeholder="YYYY-MM-DD">
+    <button class="submit-btn">space out.</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "DateForm",
+}
+</script>
+
+<style scoped>
+  .date-form {
+    margin-top: 20px;
+  }
+</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="header">
+    <h1>far out.</h1>
+    <p>your universe. daily.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
