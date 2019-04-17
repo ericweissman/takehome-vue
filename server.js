2f1e5fb14694fc22b770b1989d68952409ca284e
Eric Weissman <eric.weissman21@gmail.com>
2019-04-17 09:49:56 -0600

    Create BE express server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+require('dotenv').config();
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
+console.log('SECRET_API_KEY: ', process.env.VUE_APP_APIKEY);
